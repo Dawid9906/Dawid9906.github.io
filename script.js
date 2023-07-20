ebed843f2dd4ba514a80516fb87eaffdaf9febc3
Dawid9906 <dawid.kargul1@gmail.com>
2023-07-20 16:17:51 +0200

    cookies i footer

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ toggleBTN.addEventListener("click", () => {
 links.addEventListener("click", () => {
   links.classList.remove("active");
 });
+
+getCookie = (cName) => {
+  const name = cName + "=";
+  const cDecoded = decodeURIComponent(document.cookie);
+  const cArr = cDecoded.split("; ");
+  let value;
+  cArr.forEach((val) => {
+    if (val.indexOf(name) === 0) value = val.substring(name.length);
+  });
+  return value;
+};
+
+setCookie = (cName, cValue, expDays) => {
+  let date = new Date();
+
+  date.setTime(date.getTime() + expDays * 24 * 60 * 60 * 1000);
+  const expires = "expires" + date.toUTCString();
+  document.cookie = cName + "=" + cValue + ";" + expires + "; path=/";
+};
+
+document.querySelector("#cookies-btn").addEventListener("click", () => {
+  document.querySelector("#cookies").style.display = "none";
+  setCookie("cookie", true, 30);
+});
+
+cookieMessage = () => {
+  if (!getCookie("cookie"))
+    document.querySelector("#cookies").style.display = "block";
+};
+
+window.addEventListener("load", cookieMessage);
